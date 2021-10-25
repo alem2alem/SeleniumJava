6994d42a8470f9e1022e3913d755ae2bcbf8911d
Jemal <Jemal@DESKTOP-R9V2GFE.lan>
2021-10-25 11:03:14 -0700

    inneitial commit for assisgnemnennt

--- Project1/src/test/java/Pack1/AmazonDrop.java
+++ Project1/src/test/java/Pack1/AmazonDrop.java
@@ -30,13 +30,14 @@ public class AmazonDrop
 	public void launchapplication() throws IOException
 
 	{
-		
-		File f1=new File("C:\\Users\\Jemal\\Jamel_selenium_project\\Selenium_setup\\src\\app.properties");
-		FileReader ff=new FileReader(f1);
-		Properties pp=new Properties();//////////cell sheet , xsswoehssg
+
+		File f1 = new File("C:\\Users\\Jemal\\Jamel_selenium_project\\Selenium_setup\\src\\app.properties");
+		FileReader ff = new FileReader(f1);
+		Properties pp = new Properties();////////// cell sheet , xsswoehssg
 		pp.load(ff);///
-		System.setProperty("webdriver.chrome.driver",
-				pp.getProperty("driverpath")); //Setproperty have two attributes  propertyName and Value. sysytem.setProperty("name";"value");
+		System.setProperty("webdriver.chrome.driver", pp.getProperty("driverpath")); // Setproperty have two attributes
+																						// propertyName and Value.
+																						// sysytem.setProperty("name";"value");
 		driver = new ChromeDriver();
 		driver.navigate().to(pp.getProperty("URL"));// QA , UAT , Productio ,need to
 		driver.manage().window().maximize();
@@ -52,99 +53,102 @@ public class AmazonDrop
 		Select ss = new Select(drop); // predefined class ,, accepts webelemnt as inut
 		// ss.selectByIndex(11);
 		// ss.selectByVisibleText("Baby");
-		String input="Baby";
+		String input = "Baby";
 		ss.selectByValue("search-alias=baby-products");
-		
-		WebElement selected=ss.getFirstSelectedOption();
-		String actual=selected.getText();// Baby 
-		Assert.assertEquals(actual.trim(),  input.trim(),"both are not equal meaning selected dropdown is not dispayed");// pass  //fail
-		//Assert.assertEquals(actual.trim(), input.trim());
+
+		WebElement selected = ss.getFirstSelectedOption();
+		String actual = selected.getText();// Baby
+		Assert.assertEquals(actual.trim(), input.trim(),
+				"both are not equal meaning selected dropdown is not dispayed");// pass //fail
+		// Assert.assertEquals(actual.trim(), input.trim());
 		System.out.println(" line number 62 after assertion ");
 		System.out.println(" last line ");
-	Assert.assertEquals("Jamel","Jamel");		//
-		
-		
-		
+		Assert.assertEquals("Jamel", "Jamel"); //
+
 		///
 	}//
 
-	@Test(priority = 2,enabled = false)
+	@Test(priority = 2, enabled = false)
 	public void searchsomething_dontrun() {
 		WebElement searchinputbox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 
 		if (searchinputbox.isEnabled()) {
 			searchinputbox.sendKeys("mobile");
-			
-			//searchinputbox.clear();
+
+			// searchinputbox.clear();
 		}
 		WebElement SearchBox = driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]"));
-		
-		if (SearchBox.isDisplayed()) 
-		{
-		SearchBox.click();
-		// we will getting the results 
-		// we need to pass the testcases 
+
+		if (SearchBox.isDisplayed()) {
+			SearchBox.click();
+			// we will getting the results
+			// we need to pass the testcases
+		} else {
+			Assert.assertTrue(false, "searcbox is not displayed");
 		}
-		else
-		{
-		Assert.assertTrue(false,"searcbox is not displayed");	
-		}
-		
+
 	}
-	
-	
-	
-	@Test(priority = 3,groups= {"search"},dependsOnMethods = {"dropdownvalidation"})
+
+	@Test(priority = 3, groups = { "search" }, dependsOnMethods = { "dropdownvalidation" })
 	public void searchsomething_3() {
 		WebElement searchinputbox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 
 		if (searchinputbox.isEnabled()) {
 			searchinputbox.sendKeys("mobile");
-			
-			//searchinputbox.clear();
+
+			// searchinputbox.clear();
 		}
 		WebElement SearchBox = driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]"));
-		
-		if (SearchBox.isDisplayed()) 
-		{
-		SearchBox.click();
-		// we will getting the results 
-		// we need to pass the testcases 
+
+		if (SearchBox.isDisplayed()) {
+			SearchBox.click();
+			// we will getting the results
+			// we need to pass the testcases
+		} else {
+			Assert.assertTrue(false, "searcbox is not displayed");
 		}
-		else
-		{
-		Assert.assertTrue(false,"searcbox is not displayed");	
-		}
-		
+
 	}
-	@Test(priority = 3,groups = {"search"})
+
+	@Test(priority = 3, groups = { "search" })
 	public void searchsomething_order() {
 		WebElement searchinputbox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 
 		if (searchinputbox.isEnabled()) {
 			searchinputbox.sendKeys("mobile");
-			
-			//searchinputbox.clear();
+
+			// searchinputbox.clear();
 		}
 		WebElement SearchBox = driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]"));
-		
-		if (SearchBox.isDisplayed()) 
-		{
-		SearchBox.click();
-		// we will getting the results 
-		// we need to pass the testcases 
+
+		if (SearchBox.isDisplayed()) {
+			SearchBox.click();
+			// we will getting the results
+			// we need to pass the testcases
+		} else {
+			Assert.assertTrue(false, "searcbox is not displayed");
 		}
-		else
-		{
-		Assert.assertTrue(false,"searcbox is not displayed");	
-		}
-		
+
 	}
+	
+	//new testcase here 
+	@Test(priority = 12, groups = { "orders" })
+	public void Validatecancelorder()
+	{
+		
+		System.out.println(" order cancelltion code here ");
+		
+		//weblement 
+		
+		// webelemnt 
+		// assertion 
+	}
+
 	@AfterMethod
 	public void closebrowser()
 
 	{
-		
+
 		driver.close();
 
 	}
